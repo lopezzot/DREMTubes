27c2417569a0348911f93b9a64bce5f0de8376bc
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2021-08-24 11:30:40 +0200

    Adding TBDataAnalysis folder

--- TBDataAnalysis/202108_SPS/PhysicsAnalysis.C
+++ TBDataAnalysis/202108_SPS/PhysicsAnalysis.C
@@ -1,19 +1,22 @@
 //**************************************************
 // \file PhysicsAnalysis.C
-// \brief: analysis of Event objects
+// \brief: template analysis of Event objects
+//         example on how to perform an analysis
+//         on physics_sps2021_*.root data
 // \author: Lorenzo Pezzotti (CERN EP-SFT-sim) @lopezzot
-// \start date: 20 August 2021
+// \start date: 24 August 2021
 //**************************************************
 
 #include <TTree.h>
 #include <TFile.h>
+#include <TH2F.h>
 #include <iostream>
 #include <array>
 #include <stdint.h>
 #include <string>
-#include "json.hpp"
 #include <fstream>
-#include "PhysicsEvent.h"
+#include "../../TBDataPreparation/202108_SPS/scripts/json.hpp"
+#include "../../TBDataPreparation/202108_SPS/scripts/PhysicsEvent.h"
 
 ClassImp(EventOut)
 
@@ -26,15 +29,23 @@ void PhysicsAnalysis(const string run){
 
   auto file = new TFile(cinfile);
   auto *tree = (TTree*) file->Get("Ftree");
-  auto evt = new Event();
+  auto evt = new EventOut();
   tree->SetBranchAddress("Events",&evt);
-  double ene = 0;
+  
+  float energyS = 0;
+  float energyC = 0;
+  auto enesplot = new TH2F("splot", "splot", 100, 0., 100., 100, 0., 100.);
+
   for (unsigned int i=0; i<tree->GetEntries(); i++){
       tree->GetEntry(i);
-      evt->CompSPMTene();
-      evt->CompCPMTene();
-      std::cout<<" SPMTenergy "<<evt->SPMTenergy<<" CPMTenergy "<<evt->CPMTenergy<<std::endl;
+      if (evt->PShower>1000){
+      	  energyS = evt->totSiPMSene + evt->SPMTenergy;
+      	  energyC = evt->totSiPMCene + evt->CPMTenergy;
+      	  enesplot->Fill(energyS, energyC);
+      }
   }
+
+  enesplot->Draw("COLZ");
 
 }
 
