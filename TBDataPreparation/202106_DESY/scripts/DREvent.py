1c22a0b75fc51520b04559a65bfe2d28f0d1f695
gaudio74 <gabri.gaudio@gmail.com>
2021-11-16 10:54:28 +0100

    added script directory for creating raw Ntuple for PMT data

---
+++ TBDataPreparation/202106_DESY/scripts/DREvent.py
@@ -0,0 +1,103 @@
+# Conversion fron ascii data format to DREvent class
+
+class DREvent:
+  ''' Class that represent a Dual Readout event at TB 2021 @DESY '''
+  
+  def __init__(self):
+    ''' Constructor '''
+    self.EventNumber = 0
+    self.NumOfPhysEv = 0
+    self.NumOfPedeEv = 0
+    self.NumOfSpilEv = 0 
+    self.TriggerMask = 0
+    self.ADCs = {}        # Simple dict      key(channel) : value
+    self.TDCs = {}        # Dict key:tuple   key(channel) : ( value, check )
+
+  def headLine(self):
+    """Write header in ascii data dump"""
+    return "%8s, %8s, %8s, %8s, %10s, %6s, %6s" % (
+           "evNum", "#phEv", "#peEv", "#spEv", "trigM",
+           "#ADCs", "#TDCs", )
+  
+  def __str__(self):
+    """Overload of str operator (for data dump)"""
+    return "%8d, %8d, %8d, %8d, 0x%08x, %6d, %6d" % (
+          self.EventNumber, self.NumOfPhysEv, self.NumOfPedeEv, self.NumOfSpilEv, self.TriggerMask,
+          len(self.ADCs), len(self.TDCs) )
+
+  def getAdcChannel(self, ch):
+    """get the data value for ADC channel ch"""
+    return self.ADCs[ch]
+
+  def getTdcChannel(self, ch):
+    """get the data value for TDC channel ch"""
+    return self.TDCs[ch]  # Tuple: (value, check)
+
+# Parse the evLine and return a DREvent object
+def DRdecode(evLine):
+  """Function that converts a raw data record (event) from
+     ascii to object oriented representation: DREvent class"""
+  # Create new DREvent
+  e = DREvent()
+
+  # Split sections
+  strHeader  = evLine.split(":")[0]
+  strPayload = evLine.split(":")[1]
+  strPayload = strPayload.replace("TDC", ":TDC")
+  strADCs    = strPayload.split(":")[0]
+  strTDCs    = strPayload.split(":")[1]
+	
+  # Parse strHeader
+  hList = strHeader.split()
+  e.EventNumber = int( hList[2] )
+  e.NumOfPhysEv = int( hList[5] )
+  e.NumOfPedeEv = int( hList[6] )
+  e.NumOfSpilEv = int( hList[7] )
+  try:
+    e.TriggerMask = int( hList[10], 16 )
+  except ValueError:
+    print 'ERROR: INVALID TRIGGER MASK:', hList[10] 
+    e.TriggerMask = 0xFFFFFFFF 
+   
+  # Parse ADC 
+  listADCs = strADCs.split()
+  for i in range(len(listADCs)):
+    if i%2 ==0:
+      ch = int(listADCs[i]  , 10)
+      val= int(listADCs[i+1], 16)
+      e.ADCs[ch]=val 
+
+  # Parse TDC  
+
+  strTDCs  = strTDCs[ strTDCs.find("val.s") + 6 : ]
+  listTDCs = strTDCs.split()
+  if len(listTDCs)>0:
+    for i in range(len(listTDCs)):
+      if i%3 ==0:
+        ch  = int( listTDCs[i+0], 10 )  # Channel
+        ver = int( listTDCs[i+1], 10 )  # Varification number
+        val = int( listTDCs[i+2], 10 )  # Value
+        e.TDCs[ch] = ( val, ver) 
+  return e
+
+
+# Main for testing purpose
+if __name__ == "__main__":
+  import sys
+  if len(sys.argv) < 2:
+    print "Usage:", sys.argv[0], "filename [v=verbose]"
+ 
+  verbose = False
+  if len(sys.argv) == 3:
+    verbose = True 
+  for i, line in enumerate( open( sys.argv[1] ) ):
+    ev = DRdecode(line)
+    if verbose:
+      if i%30 == 0:
+        print ev.headLine() 
+	print ev.getAdcChannel(0)
+	print ev.getAdcChannel(16)
+      print ev
+    
+
+  
---
+++ TBDataPreparation/202106_DESY/scripts/DRrootify.py
@@ -0,0 +1,109 @@
+##**************************************************
+## \file DRrootify.py
+## \brief: converter from ASCII files to EventDR objects 
+##         to ROOT files
+## \author: Lorenzo Pezzotti (CERN EP-SFT-sim) @lopezzot
+##          Andrea Negri (UniPV & INFN)
+##	    Gabriella Gaudio (INFN PV)	
+## \start date: 16 August 2021
+##**************************************************
+
+import DREvent
+from ROOT import *
+from array import array
+import sys
+import glob
+import os
+
+class DRrootify:
+    '''Class to rootify raw ASCII files'''
+
+    def __init__(self, fname):
+        '''Class Constructor'''
+        self.drfname = fname+".txt"
+        self.drfile = TFile(fname+".root","RECREATE")
+        self.tbtree = TTree("DESY2021","DESY2021")
+        self.EventNumber = array('i',[0])
+        self.NumOfPhysEv = array('i',[0])
+        self.NumOfPedeEv = array('i',[0])
+        self.NumOfSpilEv = array('i',[0])
+        self.TriggerMask = array('l',[0])
+        self.ADCs = array('i',[-1]*32) 
+        self.TDCsval = array('i',[-1]*48)
+        self.TDCscheck = array('i',[-1]*48)
+
+        self.tbtree.Branch("EventNumber",self.EventNumber,'EventNumber/I')
+        self.tbtree.Branch("NumOfPhysEv",self.NumOfPhysEv,'NumOfPhysEv/I')
+        self.tbtree.Branch("NumOfPedeEv",self.NumOfPedeEv,'NumOfPedeEv/I')
+        self.tbtree.Branch("NumOfSpilEv",self.NumOfSpilEv,'NumOfSpilEv/I')
+        self.tbtree.Branch("TriggerMask",self.TriggerMask,'TriggerMask/L')
+        self.tbtree.Branch("ADCs",self.ADCs,'ADCs[32]/I')
+        self.tbtree.Branch("TDCsval",self.TDCsval,'TDCsval[48]/I')
+        self.tbtree.Branch("TDCscheck",self.TDCscheck,'TDCscheck[48]/I')
+
+    def ReadandRoot(self):
+        '''Read ASCII files line by line and rootify'''
+        print "--->Start rootification of "+self.drfname
+        for i, line in enumerate(open(self.drfname)):
+            if i%5000 == 0 : print "------>At line "+str(i)+" of "+str(self.drfname)
+            evt = DREvent.DRdecode(line) 
+            self.EventNumber[0] = evt.EventNumber
+            self.NumOfPhysEv[0] = evt.NumOfPhysEv
+            self.NumOfPedeEv[0] = evt.NumOfPedeEv
+            self.NumOfSpilEv[0] = evt.NumOfSpilEv
+            self.TriggerMask[0] = evt.TriggerMask
+	    #if evt.TriggerMask > 10:
+	    #	print evt.TriggerMask
+            for counter, l in enumerate(evt.ADCs.items()):
+                self.ADCs[counter] = l[1]
+            for counter, l in enumerate(evt.TDCs.items()):
+                self.TDCsval[counter] = l[1][0]
+                self.TDCscheck[counter] = l[1][1]
+            self.tbtree.Fill()
+        print "--->End rootification of "+self.drfname
+    
+    def Write(self):
+        self.tbtree.Write()
+        self.drfile.Close()
+
+#Quick test me
+#
+#fname = "/afs/cern.ch/user/g/gaudio/data/testroot_desy/desydata.run309"
+#dr = DRrootify(fname)
+#dr.ReadandRoot()
+#dr.Write()
+
+#Get list of files on rawData and rawNtuple
+#
+datapath = "/eos/user/i/ideadr/TB2021_Desy/rawData/"
+ntuplepath = "/eos/user/i/ideadr/TB2021_Desy/rawNtuple/"
+datafls = [x.split(".bz2")[0] for x in glob.glob(datapath+"desydata*.bz2")]
+datafls = [os.path.basename(x) for x in datafls]
+ntuplfls = [x.split(".root")[0]+".txt" for x in glob.glob(ntuplepath+"*.root")]
+ntuplfls = [os.path.basename(x) for x in ntuplfls]
+newfls = list(set(datafls)-set(ntuplfls))
+
+#Rootify those data
+#
+print "Hi!"
+if newfls:
+    print str(len(newfls))+" new files found"
+#newfls = ["sps2021data.run627.txt"]
+for fl in newfls:
+    print "->Found new file to be rootified: "+str(fl)
+    os.system("bzip2 -d -k "+datapath+str(fl)+".bz2")
+    print "--->"+str(fl)+".bz2 decompressed"
+    fname = fl[0:-4]
+    dr = DRrootify(datapath+fname)
+    dr.ReadandRoot()
+    dr.Write()
+    os.system("rm "+datapath+fl)
+    os.system("mv "+datapath+str(fl[0:-4])+".root "+ntuplepath) 
+
+#If no new files found
+#
+if not newfls:
+    print "->No new files found"
+print "Bye!"
+
+##**************************************************
